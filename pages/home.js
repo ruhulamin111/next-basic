36e50caa26d0dae0c01137acccee08aaf0abcdd4
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-26 11:31:32 +0600

    next js start

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>home</h1>
+      <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos tempore adipisci ducimus vero sequi ratione fugiat reiciendis distinctio unde nobis!</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut quaerat sapiente ipsa reiciendis dolore rem optio commodi similique soluta molestiae numquam quibusdam, culpa tenetur magnam, possimus voluptatibus officia porro sequi eius hic. Voluptate nesciunt distinctio, harum expedita dolor aliquid deserunt cupiditate reiciendis consequuntur quibusdam suscipit! Aliquam dolore maiores eveniet.</p>
+    </div>
+  )
+}
